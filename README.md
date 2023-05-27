fde48e0556a4ae034a5aaeddeec5a84b04eba136
Vanderlei Geronimo Junior <vand.9jr@gmail.com>
2023-05-27 09:33:58 -0300

    Desenvolvimento dos botões de controle barra de progresso do player em css

--- README.md
+++ README.md
@@ -12,25 +12,32 @@
 - [Autor](#autor)
 
 
-## Overview
 ### Links
+- https://fonts.google.com/
+- https://boxicons.com/?query=
+- https://placehold.it/400
 ## Meu processo
 
 ### Construído com
 
-- HTML5 semântica
-- CSS 
+- HTML5
+- CSS3
 - JavaScript
 
 ### O que eu aprendi
 
 - Manipular Html
+- Estilizar botões de um player de música
 
 ### Desenvolvimento contínuo
+
+- Posso implementar várias outras funcionalidades aqui como, 
 
 ### Recursos úteis
 
 - Google Fonts - https://fonts.google.com/
+- Imagem de fundo - https://placehold.it/400 "O Tamanho da Imagem é alterado após a ultima barra."
+- Icones Boxicons - https://boxicons.com/?query=
 
 ## Autor
 
---
+++ js/main.js
@@ -0,0 +1,27 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctrl_play = document.getElementById("ctrl_play");
+
+song.onloadedmetadata = function(){
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+function playPause(){
+    if(ctrl_play.classList.contains("bx-pause")){
+        song.pause();
+        ctrl_play.classList.remove("bx-pause");
+        ctrl_play.classList.add("bx-play");
+    }
+    else{
+        song.play();
+        ctrl_play.classList.add("bx-pause");
+        ctrl_play.classList.remove("bx-play");
+    }
+}
+
+if(song.play()){
+    setInterval(() => {
+        progress.value = song.currentTime;
+    },500),
+}
